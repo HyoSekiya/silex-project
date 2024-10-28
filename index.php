7bb811c9119a123bd938ee9190ed2cb7dd528471
HyoSekiya <19aj083@ms.dendai.ac.jp>
2024-10-29 00:14:51 +0900

    restfull仕様

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $app = new Silex\Application();
 $app->get('/', function () {
     return '
         <h1>Input Form</h1>
-        <form action="/submit" method="POST">
+        <form action="/hello/name" method="POST">
             <label for="name">Enter your name:</label>
             <input type="text" id="name" name="name" required>
             <button type="submit">Submit</button>
@@ -18,9 +18,16 @@ $app->get('/', function () {
 });
 
 // フォームデータを処理するページ
-$app->post('/submit', function () use ($app) {
+$app->post('/hello/name', function () use ($app) {
     // PHPのスーパーグローバル $_POST を使用してデータを取得
     $name = isset($_POST['name']) ? $_POST['name'] : 'Guest';
+
+    // リダイレクトして、GETリクエストで名前をURIに埋め込む
+    return $app->redirect("/hello/name/$name");
+});
+
+// 名前を表示するページ
+$app->get('/hello/name/{name}', function ($name) use ($app) {
     return '<h1>Hello, ' . htmlspecialchars($name) . '!</h1>';
 });
 
